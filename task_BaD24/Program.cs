bea8ea8dac4272592bb89a2854327373d8ebbc6d
Vitalii Udod <vitalii.udod@gmail.com>
2024-03-05 13:56:01 +0200

    Add project files.

---
+++ task_BaD24/Program.cs
@@ -0,0 +1,95 @@
+﻿#pragma warning disable S2583 // Conditionally executed code should be reachable
+#pragma warning disable S6561 // Avoid using "DateTime.Now" for benchmarking or timing operations
+namespace task_BaD24
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(
+                "------------------------------------------- \n" +
+                "  Test task for PortaOne BaD_2024 program.\n" +
+                "  Write by Vitalii Udod. \n" +
+                "-------------------------------------------");
+
+            DateTime start = DateTime.Now;
+            var testList = new TestBaD24(new List<int>());
+            
+            /// Reading data_file
+            if (args.Length > 0)
+            {
+                Console.Write($"Reading file: {args[0]}\t");
+                var listInt = new List<int>(GetListInt(args[0]));
+                if (listInt.Count > 0)
+                {
+                    testList = new TestBaD24(listInt);
+                    Console.WriteLine($"\nElements: {testList.GetList.Count:N0}");
+                }
+            }
+            else
+            {
+                Console.WriteLine(" No datafile! \n    USAGE: \n\ttask_BaD24 <int_list_file.txt> \n\nFor example: \n\ttask_BaD24 10m.txt \n");
+            }
+            
+            /// if listInt != 0 :
+            if (testList.GetList.Count > 0)
+            {
+                Console.WriteLine("-------------------------------------------");
+                Console.WriteLine(
+                    $"1. Int Max: {testList.ListMax} \n" +
+                    $"2. Int Min: {testList.ListMin} \n" +
+                    $"3. Mediana: {testList.Mediana} \n" +
+                    $"4. Avarage: {testList.ListAvg:F2} \n" +
+                    $"5. INCREASES_sequence: {GetListString(testList.GetIncrSeq())} \n" +
+                    $"6. DECREASING_sequence: {GetListString(testList.GetDecrSeq())}"
+                    );
+                Console.WriteLine("-------------------------------------------");
+
+                TimeSpan stop = DateTime.Now - start;
+                Console.WriteLine($"Total time, sec: {stop.TotalSeconds:f4}");
+            }
+        }
+
+        static List<int> GetListInt(string fileName)
+        {
+            var intList = new List<int>();
+            var dataFile = new FileInfo(fileName);
+            try
+            {
+                using (StreamReader sr = dataFile.OpenText())
+                {
+                    string dataStr = sr.ReadToEnd();
+                    List<string> strList = dataStr.Trim().Split("\n").ToList();
+                    foreach (string str in strList)
+                        if (str != "" || str != " ")
+                            intList.Add(int.Parse(str));
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine($"ERROR! \n{ex.Message}");
+                ///throw new FileNotFoundException(ex.Message);
+                return new List<int>();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"ERROR! \nBad data in file: {fileName} \n{ex.Message}");
+                ///throw new FormatException(ex.Message);
+                return new List<int>();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR! \n" + ex.Message);
+                return new List<int>();
+            }
+
+            return intList;
+        }
+
+        static string GetListString(List<int> intList)
+        {
+            return string.Join(" ", intList.Select(x => x.ToString()).ToList());
+        }
+
+    }
+}
---
+++ task_BaD24/TestBaD24.cs
@@ -0,0 +1,93 @@
+﻿#pragma warning disable S2583 // Conditionally executed code should be reachable
+#pragma warning disable S6608 // Prefer indexing instead of "Enumerable" methods on types implementing "IList"
+namespace task_BaD24
+{
+    internal class TestBaD24
+    {
+        private readonly List<int> listInt;
+        public List<int> GetList { get => listInt; }
+        public int ListMax { get; private set; }
+        public int ListMin { get; private set; }
+        public double ListAvg { get; private set; }
+        public double Mediana { get => GetMediana(); }
+
+        public TestBaD24(List<int> _listInt)
+        {
+            listInt = new List<int>(_listInt);
+            if (listInt.Count > 0)
+            {
+                ListMin = listInt.Min();
+                ListMax = listInt.Max();
+                ListAvg = listInt.Average();
+            }
+        }
+
+        private double GetMediana()
+        {
+            var _listInt = new List<int>(listInt);
+            _listInt.Sort();
+
+            if ((_listInt.Count % 2) == 0)
+            {
+                int index2 = _listInt.Count / 2;
+                int index1 = index2 - 1;
+                return (double)(_listInt[index1] + _listInt[index2]) / 2;
+            }
+            else
+            {
+                int index = _listInt.Count / 2;
+                return (double)(_listInt[index]);
+            }
+        }
+
+        public List<int> GetIncrSeq()
+        {
+            var incrSeq = new List<int>();
+            var tmpSeq = new List<int>();
+
+            foreach (int itemList in listInt)
+            {
+                if (tmpSeq.Count == 0 || tmpSeq.Last() <= itemList)
+                {
+                    tmpSeq.Add(itemList);
+                }
+                else if (tmpSeq.Count >= incrSeq.Count)
+                {
+                    incrSeq = tmpSeq;
+                    tmpSeq = new List<int> { itemList };
+                }
+                else
+                {
+                    tmpSeq = new List<int> { itemList };
+                }
+            }
+
+            return incrSeq;
+        }
+
+        public List<int> GetDecrSeq()
+        {
+            var decrSeq = new List<int>();
+            var tmpSeq = new List<int>();
+
+            foreach (int itemList in listInt)
+            {
+                if (tmpSeq.Count == 0 || tmpSeq.Last() >= itemList)
+                {
+                    tmpSeq.Add(itemList);
+                }
+                else if (tmpSeq.Count >= decrSeq.Count)
+                {
+                    decrSeq = tmpSeq;
+                    tmpSeq = new List<int> { itemList };
+                }
+                else
+                {
+                    tmpSeq = new List<int> { itemList };
+                }
+            }
+
+            return decrSeq;
+        }
+    }
+}
